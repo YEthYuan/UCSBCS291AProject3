3e06db653911840900cad74d87e2bd3127804d45
yuanye <yethyuan@gmail.com>
2022-10-09 22:50:04 -0700

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,14 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "posts#index"
+
+  resources :users do
+    resources :posts do
+      resources :comments
+    end
+  end
+
+  resources :posts
+  resources :comments
 end
