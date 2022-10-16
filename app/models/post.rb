66b48b96a2430a2fb6712ce5227e1bb773bd0cb2
AndyCheng2 <luqingcheng2@gmail.com>
2022-10-16 16:36:58 -0700

    fix: comment_destroy_modify

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :body, presence: true
   # validates :user_id, presence: true, inclusion: { in: User.all,
   #                                               message: "User id %{value} is not in user's list"}
