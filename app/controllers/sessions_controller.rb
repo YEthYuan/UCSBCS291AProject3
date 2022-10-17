6f156754789c5f9903238a03d24a1c3f593b3385
yuanye <yethyuan@gmail.com>
2022-10-17 00:24:53 -0700

    debug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,11 @@
 class SessionsController < ApplicationController
 
   def new
+
   end
 
   def create
-    user = User.find_by(name: params[:session][:name].downcase)
+    user = User.find_by(name: params[:session][:name])
     if user
       current_user
       log_in user
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,7 +6,7 @@ module SessionsHelper
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
-    puts(:current_user)
+    puts " ++DEBUG++ #{@current_user}"
   end
 
   def logged_in?
